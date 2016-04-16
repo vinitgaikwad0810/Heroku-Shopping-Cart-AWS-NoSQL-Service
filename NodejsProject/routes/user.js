df1d622fd03035c53d486e5cc5e61cd97e2c6bb4
jagrutipatil <jagrutipatil32@gmail.com>
2016-04-15 21:06:01 -0700

    closes #5

--- NodejsProject/routes/user.js
+++ NodejsProject/routes/user.js
@@ -1,8 +1,16 @@
 
-/*
- * GET users listing.
- */
+var redis = require("redis")
+var client = redis.createClient()
+
+
+function redisTest() {
+	client.set('test', 'Jagruti');
+	console.log(client.get('test'));
+}
+
 
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+exports.redisTest = redisTest; 
