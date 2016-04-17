60b320260e74dd8a5496d6a1563872d1d7f4a9f1
Faisal9227 <m.faisal.92.27@gmail.com>
2016-04-16 18:04:04 -0700

    Update Cass_Delete.js
    File updated

--- Cassandra/Cass_Delete.js
+++ Cassandra/Cass_Delete.js
@@ -1,20 +1,19 @@
 var cassandra = require('cassandra-driver');
 var async = require('async');
-var client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'db275'});
+var client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'db281'});
 function select(callback) {
        client.execute("DELETE FROM test WHERE id = 2", function () {
            if (!err){
-               if ( result.rows.length > 0 ) {
-                   var row = result.rows[0];
-                   console.log("id = %d, name = %d", row.id, row.name);
-               } else {
-                   console.log("No rows selected");
-               }
+                   console.log("Error");
+           	}
+           else{
+           	 console.log("Sucess");
+           }
            }
            callback();
        });
    }
 function funcSelect(){
-	console.log("Select Query");
+	console.log("Delete Query");
 }
 select(funcSelect);
