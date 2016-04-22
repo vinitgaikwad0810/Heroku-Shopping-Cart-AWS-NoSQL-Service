853f0a117b2d70ad685a2ae62cb6fff39e34b9ad
Faisal <m.faisal.92.27@gmail.com>
2016-04-21 19:46:09 -0700

    Restful

---
+++ Cassandra/Cassandra Connection/routes/Server.js
@@ -0,0 +1,5 @@
+var http=require("http");
+var server = http.createServer(function(request, response){
+	response.writeHead(200,{"Content-Type":"text/html"});
+	response.end("Hi CMPE281");
+}).listen(8888);
