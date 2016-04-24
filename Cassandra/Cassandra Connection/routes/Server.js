274f15400638cee71cf4ba59736abfbe984528c4
Faisal9227 <m.faisal.92.27@gmail.com>
2016-04-23 19:05:13 -0700

    Update Server.js
    JSON Manipulation using NodeJS

--- Cassandra/Cassandra Connection/routes/Server.js
+++ Cassandra/Cassandra Connection/routes/Server.js
@@ -1,5 +1,69 @@
-var http=require("http");
-var server = http.createServer(function(request, response){
-	response.writeHead(200,{"Content-Type":"text/html"});
-	response.end("Hi CMPE281");
-}).listen(8888);
+var express = require('express');
+var app = express();
+var fs = require("fs");
+var user = {
+   "user4" : {
+      "name" : "zxc",
+      "password" : "password4",
+      "profession" : "bgt",
+      "id": 4
+   }
+};
+// GET request
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+});
+
+//Hardcoded user addition
+app.get('/addUser', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       data.user4 = user.user4;
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+});
+
+//Server Selective Get request by id
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       users = JSON.parse( data );
+       var user = users["user" + req.params.id] 
+       console.log( user );
+       res.end( JSON.stringify(user));
+   });
+});
+
+//Server Delete Operation
+var id = 2;
+
+app.get('/deleteUser', function (req, res) {
+
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       delete data["user" + 2];
+       
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+});
+
+//Server Initiation
+var server = app.listen(8888, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Example app listening at http://%s:%s", host, port);
+
+});
