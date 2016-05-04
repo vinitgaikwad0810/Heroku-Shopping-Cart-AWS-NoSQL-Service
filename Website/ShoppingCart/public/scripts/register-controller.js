e037ad28a26ad29f32c8f9d20ddd0f027c42d1ba
UnmeshDeshmukh <unmesh.deshmukh@sjsu.edu>
2016-05-03 22:07:17 -0700

    Changes in Controller Files and Successful Post

--- Website/ShoppingCart/public/scripts/register-controller.js
+++ Website/ShoppingCart/public/scripts/register-controller.js
@@ -13,15 +13,7 @@
 				method: 'POST',
 				url: 'http://52.37.112.11:8888/newuser',
 				data: {"fname" : $scope.fname,"lname" : $scope.lname, "uname" : $scope.email, "password" : $scope.password, "mobileno" : $scope.mobileno},
-				headers: {
-					'Content-Type': 'application/json',
-					'Access-Control-Allow-Origin': 'http://localhost:3000',
-					'Access-Control-Allow-Credentials': 'true',
-					'Access-Control-Allow-Methods':'POST, GET, OPTIONS, PUT, DELETE, HEAD',
-					 //'Content-Type': 'Accept'
-					},
-					crossDomain:true
-			
+				
 			}).success(function(data){
 				console.log(data);
 			})
