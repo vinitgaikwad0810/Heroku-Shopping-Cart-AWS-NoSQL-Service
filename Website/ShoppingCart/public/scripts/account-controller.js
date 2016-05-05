fb844b3269cb5eeb48ec4a91f477a38b0406ca4b
UnmeshDeshmukh <unmesh.deshmukh@sjsu.edu>
2016-05-04 18:50:31 -0700

    Login Functionality Implemented

--- Website/ShoppingCart/public/scripts/account-controller.js
+++ Website/ShoppingCart/public/scripts/account-controller.js
@@ -1,17 +1,21 @@
 (function(){
 	var app = angular.module("accApp",[]);
 	
-	var accController = function($scope, $http){
+	var accController = function($scope, $http,$window){
 		
 		$scope.Login = function(){
 			console.log($scope.email, $scope.password);
 			
 			$http({
 				method: 'POST',
-				url: '/login',
-				data: {"email" : $scope.email, "password" : $scope.password}
+				url: 'http://52.37.112.11:8888/getpassword',
+				data: {"uname" : $scope.email, "password" : $scope.password}
 			}).success(function(data){
-				console.log(data);
+				if(data.result==="success"){
+					$window.location.assign('/');
+				}else{
+					alert("Error!! Invalid Username or Password");
+				}
 			})
 			
 		}
