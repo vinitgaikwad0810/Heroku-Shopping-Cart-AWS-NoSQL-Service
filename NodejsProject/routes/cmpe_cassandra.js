093536c53535f2c9d4298e1aacd6771cde329ead
Vinit Anil Gaikwad <vinit.gaikwad10@gmail.com>
2016-05-06 20:10:20 -0700

    Cassandra bug fix

--- NodejsProject/routes/cmpe_cassandra.js
+++ NodejsProject/routes/cmpe_cassandra.js
@@ -4,12 +4,18 @@ var request = require('sync-request');
 
 function log(req,res){
 	console.log(req.body);	
+	/*
 	var json = {
 		    "level" : "debug",
 		    "message" : "Hi"
-		};
+		};*/
+
+	var	json = {};
+	json["level"] = "debug";
+	json["message"] = "Hi";	
 	
-	var httpcall = request('POST', 'http://52.24.140.37:8888/log', json);		
+
+	var httpcall = request('POST', 'http://52.24.140.37:8888/log', {json: { data :JSON.stringify(json)}});		
 	res.end();
  }
 
