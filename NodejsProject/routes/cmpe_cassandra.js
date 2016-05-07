a187ec2db8ce787116f4c807bb23e92f1fcc33fb
Vinit Anil Gaikwad <vinit.gaikwad10@gmail.com>
2016-05-06 20:15:31 -0700

    Cassandra bug fix

--- NodejsProject/routes/cmpe_cassandra.js
+++ NodejsProject/routes/cmpe_cassandra.js
@@ -1,19 +1,4 @@
 
-var log4js = require('log4js');
-log4js.configure({
-	  appenders: [ 
-	    { 
-	      type:     'log4js-appender-cassandra',
-	      nodes:    ['52.24.140.37'],
-	      username: 'root',
-	      password: 'root',
-	      keyspace: 'test',
-	     // port:		"7000",
-	      table:    'system_logs'
-	      }
-	    ]
-	});
-
 var https = require("https");
 var request = require('sync-request');
 
@@ -34,4 +19,4 @@ function log(req,res){
 	res.end();
  }
 
-exports.log=log;
+exports.log=log;	
